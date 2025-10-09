a3ed1f1e0a6dfec99cc9867d22ee60062d7c5f8c
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-10-09 10:20:43 +0200

    Update option j/k keymap to move lines up and down

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move selected code block down with Option+j (which sends √)
+vim.keymap.set("v", "√", ":m '>+1<CR>gv=gv")
+
+-- Move selected code block up with Option+k (which sends ª)
+vim.keymap.set("v", "ª", ":m '<-2<CR>gv=gv")
